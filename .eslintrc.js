d6719df1a48fd4122034535ab76e780ec4e06457
wesley sun <sunwes@gmail.com>
2026-08-13 21:52:11 -0700

    chore: modernize npm setup and ci

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,31 @@
 module.exports = {
-  "root": true,
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": { "project": "./tsconfig.json" },
-  "env": { "es6": true },
-  "ignorePatterns": ["node_modules", "build", "coverage"],
-  "plugins": ["import", "eslint-comments", "functional"],
-  "extends": [
-    "eslint:recommended",
-    "plugin:eslint-comments/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/typescript",
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: { project: './tsconfig.json' },
+  env: { es6: true },
+  ignorePatterns: ['node_modules', 'build', 'coverage'],
+  plugins: ['import', 'eslint-comments', 'functional'],
+  extends: [
+    'eslint:recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
     // "plugin:functional/lite", // re-enable in future refactor
-    "plugin:prettier/recommended",
-    "prettier/@typescript-eslint"
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
   ],
-  "globals": { "BigInt": true, "console": true, "WebAssembly": true },
-  "rules": {
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "eslint-comments/disable-enable-pair": [
-      "error",
-      { "allowWholeFile": true }
+  globals: { BigInt: true, console: true, WebAssembly: true },
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }],
+    'eslint-comments/no-unused-disable': 'error',
+    'import/order': [
+      'error',
+      { 'newlines-between': 'always', alphabetize: { order: 'asc' } },
     ],
-    "eslint-comments/no-unused-disable": "error",
-    "import/order": [
-      "error",
-      { "newlines-between": "always", "alphabetize": { "order": "asc" } }
+    'sort-imports': [
+      'error',
+      { ignoreDeclarationSort: true, ignoreCase: true },
     ],
-    "sort-imports": [
-      "error",
-      { "ignoreDeclarationSort": true, "ignoreCase": true }
-    ]
-  }
+  },
 };
--- .github/CONTRIBUTING.md
+++ .github/CONTRIBUTING.md
@@ -1,3 +1,23 @@
-# Example Contributing Guidelines
+# Contributing
 
-This is an example of GitHub's contributing guidelines file. Check out GitHub's [CONTRIBUTING.md help center article](https://help.github.com/articles/setting-guidelines-for-repository-contributors/) for more information.
+## Prerequisites
+
+- Node.js 22 or later. Run `nvm use` to select the repository's default Node version when using nvm.
+- npm 11.2.0, as declared in `package.json`.
+
+## Setup and validation
+
+Install the locked dependency graph with:
+
+```sh
+npm ci
+```
+
+Before opening a pull request, run:
+
+```sh
+npm test
+npm run cov:check
+```
+
+`npm test` is read-only with respect to tracked source files. Use `npm run lint:fix` or `npm run format` only when you intend to apply automated fixes.
